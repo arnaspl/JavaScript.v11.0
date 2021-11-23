5f17f0b5d086b6ab556409d83487300703dd25c9
arnaspl <60697454+arnaspl@users.noreply.github.com>
2021-11-23 15:03:38 +0200

    v11.0 lesson

---
+++ main.ts
@@ -0,0 +1,37 @@
+let firstName: string = 'Edvardas';
+let age: number = 18;
+let isMale: boolean = true;
+let hobbies: any = 'string';
+
+const names: string[] = ['Petras', 'Jonas', 'Ona'];
+const numbers: number[] = [1, 2, 4, 8, 16];
+
+// Tuples
+let person: [string, number, boolean] = ['String', 16, false];
+
+// Union
+let id: string | number = 1;
+id = '1A';
+
+function add(x: number, y: number): number {
+    return x + y;
+}
+
+add(7, 2);
+
+function addNumber(x: number, y: number): void {
+    console.log(x + y);
+}
+
+const user: User = {
+    id: 1,
+    firstName: 'Jonas',
+    lastName: 'Jonaitis'
+}
+
+interface User {
+    id: number,
+    firstName: string,
+    readonly lastName: string, // Unable to change value
+    age?: number // This value is not required
+}
